45cae39f96bd0921fba9b8cc298f8b4fbb1514a3
Alejandro Nanez <git@alejandro.dev>
2022-09-15 10:29:52 -0500

    using pattern matching and structs. Code is better now.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting)
+    }
+}
+
 fn main() {
-    let visitor_list = ["bert", "steve", "fred"];
-    let mut allow_them_in = false;
+    let visitor_list = [
+        Visitor::new("bert", "hell Bert, you're great"),
+        Visitor::new("steve", "Hey Steve, how are you?"),
+        Visitor::new("fred", "Wow, you're amazing!"),
+    ];
 
     println!("Hello, what's your name?");
 
     let name = what_is_your_name();
 
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
-        }
-    }
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
 
-    if allow_them_in {
-        println!("Hello, {:?}", name)
-    } else {
-        println!("You're not allowed")
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You're not allowed!"),
     }
 }
 
